da8119d9ebac2e4f6feb31d8c92e0524fee78a61
starkpero <anishgupta0066@gmail.com>
2022-03-16 12:40:17 +0530

    created a schema to create new bootcamp

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -1,3 +1,5 @@
+const Bootcamp = require('../models/Bootcamp');
+
 // @desc    Get all bootcamps
 // @route   GET /api/v1/bootcamps
 // @access  Public  
@@ -15,9 +17,19 @@ exports.getBootCamp = (req, res, next) => {
 // @desc    Create new bootcamps
 // @route   POST /api/v1/bootcamps
 // @access  Private
-exports.createBootCamp = (req, res, next) => {
-    res.status(200).json({success: true, msg: 'Create new bootcamp'});
-}
+exports.createBootCamp = async (req, res, next) => {
+    try{
+        const bootcamp = await Bootcamp.create(req.body);
+
+        res.status(201).json({
+            success: true,
+            data: bootcamp
+        });
+    }catch(err){
+        res.status(400).json({success: false});
+    }
+    
+};
 
 // @desc    Update bootcamp
 // @route   PUT /api/v1/bootcamp/:id
--- server.js
+++ server.js
@@ -15,6 +15,8 @@ const bootcamps = require('./routes/bootcamps');
 
 const app = express();
 
+//Body parser
+app.use(express.json());
 
 // Dev logging middleware
 if(process.env.NODE_ENV === 'development'){
